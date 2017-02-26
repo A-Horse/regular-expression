c4b16fff4e14e0c4a7424644f985cdcca08511d6
fwchen <crystal.tyan@gmail.com>
2017-02-26 23:21:56 +0800

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ]
+      }
+    ]
   }
 };
